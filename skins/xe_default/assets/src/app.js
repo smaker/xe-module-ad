e8d1f5cc6cfbcac726f17c2b2c65d7323e130071
SimpleCode <dowon2308@gmail.com>
2020-11-25 14:31:29 +0900

    Desktop Skin Commit

--- skins/xe_default/assets/src/app.js
+++
@@ -1,8 +0,0 @@
-var $ = jQuery
-
-$(function() {
-    $('#ad_color, #ad_bgcolor').change(function() {
-        var color = $(this).find(':selected').val()
-        $(this).prev('.colorOverlay').css('backgroundColor', color == -1 ? "" : color )
-    });
-})
